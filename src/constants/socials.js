79d4f87768f7a456c253e2df8a4f0383524d819b
Rigan Nur Fauzi <riganfauzi@gmail.com>
2025-05-09 15:12:30 +0700

    delete

--- src/constants/socials.js
+++
@@ -1,8 +0,0 @@
-const socials = [
-    { name: "LinkedIn", url: import.meta.env.VITE_APP_OWNER_LINKEDIN_URL },
-    { name: "Twitter", url: import.meta.env.VITE_APP_OWNER_TWITTER_URL },
-    { name: "Instagram", url: import.meta.env.VITE_APP_OWNER_INSTAGRAM_URL },
-    { name: "Webflow", url: import.meta.env.VITE_APP_OWNER_WEBFLOW_URL },
-  ];
-  
-  export default socials;
