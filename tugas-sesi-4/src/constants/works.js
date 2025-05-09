a20995bc3773c3dc921a2fa737c944a39dfb87c8
Rigan Nur Fauzi <riganfauzi@gmail.com>
2025-05-09 15:47:18 +0700

    feat: update new features

---
+++ tugas-sesi-4/src/constants/works.js
@@ -0,0 +1,20 @@
+const works = [
+    {
+      name: "Decodable.co",
+      fields: ["Brand Design", "Webflow Development", "Web Design"],
+    },
+    {
+      name: "Gofirefly.io",
+      fields: ["Brand Design", "Webflow Development", "Web Design"],
+    },
+    {
+      name: "Blinkops.com",
+      fields: ["Brand Design", "Webflow Development", "Web Design"],
+    },
+    {
+      name: "Withkanvas.com",
+      fields: ["Brand Design", "Webflow Development", "Web Design"],
+    },
+  ];
+  
+  export default works;
